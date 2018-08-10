df4ca9c5c1931700583c8e844ff4fec7e50f6436
Miroslav Muras <mirekmuras@gmail.com>
2018-08-10 19:45:26 -0400

    Changed directory name and wrote compiler.js script

---
+++ compile.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname,'contracts', 'inbox.sol');
+const source = fs.readdirSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
+
