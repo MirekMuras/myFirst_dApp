e672167a341ce54d837dc52b958e0630ac62a26b
Miroslav Muras <mirekmuras@gmail.com>
2018-08-12 22:39:36 -0400

    assert test with mocha

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,33 +1,50 @@
-const assert = require('assert');                   //check-test if one value is equeal to another
+const assert = require('assert');                   //check-test/comparing  if one value is equeal to another
 const ganache = require("ganache-cli");
 const Web3 = require('web3');                       //constructor 
 const provider = ganache.provider();
 const web3 = new Web3(provider);          //instance of Web3, provider is function to comunicate with web3 and ganache 
 
-//testin framework with mocha test network , threee main functions
-//1-it function will run individual test, assertin [testing]
+//MOCHA testing framework have a three main functions
+//1-' it ' function will run one individual test, assertin [testing] on something we want to test
 
-//2-descripe function, group colectoion of 'it' functions
+//2- ' descripe ' function, group colectoion of 'it' functions
 
-//3-beforeEach function, extract some logic and run the code only one time
+//3- ' beforeEach '  function, extract some amount of logic and execute the code only one time comment to set of 'it' block
 
 class Car {
     park() {
     return 'stopped';
     }
 
-drive() {
-
-    return 'run';
+    drive() {
+        return 'run';
     }
-}
+  }
 
 //test
+let car;
+beforeEach(() => {
+    car = new Car();
+});
+
 describe('Car', () => {
-    it('testing car.park() function', () => {
+    // assert all ' it ' functions
+
+    //@dev:  test the park function
+    //@par:  will return the park function value ' stopped ' if the function is correct and called
+    it('testing car.park() function. If you can see the msg. the test passed', () => {
         //test setup with asssert logic
-        const car = new Car();
-        //checking if the car.park() return the 'stopped' valu from the function
+        
+        //const car = new Car();            -----> declare the variable and group it is ' beforeEach ' function
+        //@dev: checking if the car.park() return the 'stopped' valu from the function
+        //@par: should return ' stopped ' value if passed othervise faild the test
         assert.equal(car.park(), 'stopped');        
+    });
+
+    //@dev: tty second ' it ' test function
+    //@prov:
+    it('testing car.driver() function. If you see the msg., the assertion passed', () => {
+        //const car = new Car();
+        assert.equal(car.drive(), 'run');
     });
 });
